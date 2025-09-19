a8eb4f551d73788afef264a3d1305af43ff6e9a1
OnkarAtole <atoleonkar24@gmail.com>
2025-09-19 22:24:57 +0530

    Hosting problem

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -3,8 +3,15 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("token");
-  if (!token) return <Navigate to="/login" replace />;
-  return children;
+  // if (!token) return <Navigate to="/login" replace />;
+  // return children;
+  if (token) {
+    return children; // logged-in users see all features
+  }
+
+  // For public access: show general content
+  return children; 
+
 };
 
 export default ProtectedRoute;
